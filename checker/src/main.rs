cf97af0689fe0ef962690bcec17de64e81605788
Zahari Petkov <zahari@balena.io>
2022-09-01 11:17:18 +0300

    Checker listens on all IPs instead of localhost
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- checker/src/main.rs
+++ checker/src/main.rs
@@ -120,7 +120,7 @@ async fn main() -> std::io::Result<()> {
             .service(check_gnome)
             .service(check_fedora)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
